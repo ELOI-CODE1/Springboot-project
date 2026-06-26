0b36106a96f0f4c4977c7aa2f80af45c38c15fc4
ELOI-CODE1 <izabayoeloi2@gmail.com>
2026-06-26 18:10:24 +0200

    Initial commit for random number

---
+++ randomNumber/src/main/java/com/app2/randomNumber/RandomNumberApplication.java
@@ -0,0 +1,13 @@
+package com.app2.randomNumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomNumberApplication.class, args);
+	}
+
+}
